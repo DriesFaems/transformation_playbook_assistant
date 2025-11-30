d05423f3c2f0149a7b8574eec11b3e104f67d5de
DriesFaems <76656102+DriesFaems@users.noreply.github.com>
2025-11-30 10:07:02 +0100

    Add files via upload

--- readiness_assessment_second.py
+++ readiness_assessment_second.py
@@ -89,7 +89,7 @@ if st.button('Start Analysis'):
     GROQ_LLM = ChatGroq(
             # api_key=os.getenv("GROQ_API_KEY"),
             api_key=groq_api_key,
-            model="llama-3.1-8b-instant"
+            model="openai/gpt-oss-20b"
         )
     strategic_readiness_investigator = Agent(
         role='Investigating strategic readiness',
